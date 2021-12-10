7d30709cc9258a07d0a38bb86a14dc5d04e737ae
zwxzxz <2574802744@qq.com>
2021-12-10 11:40:15 +0800

    依赖添加&基本目录创建

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module top.zwx.crmmanage {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
+    requires hutool.all;
+    requires java.sql;
+    requires lombok;
 
 
     opens top.zwx.crm.manage to javafx.fxml;
     exports top.zwx.crm.manage;
+
 }
